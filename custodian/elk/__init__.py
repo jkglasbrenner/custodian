4b7111e5b0719526f8e9e5d9828f3b7e8789fe88
James K. Glasbrenner <jglasbr2@gmu.edu>
2018-02-03 12:16:54 -0500

    Create ELK-specific directory

---
+++ custodian/elk/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import unicode_literals
+
+"""
+This package implements various ELK Jobs and Error Handlers.
+"""
+
+__author__ = "James K. Glasbrenner"
+__version__ = "0.1"
+__maintainer__ = "James K. Glasbrenner"
+__email__ = "jglasbr2@gmu.edu"
+__status__ = "Development"
+__date__ = "2/3/18"
